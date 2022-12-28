8b41c50beaa27569a3ae93078ee94e2594e743a4
Dan Baumann <108550360+DanBaumannBB@users.noreply.github.com>
2022-12-28 14:37:59 -0800

    Install Script to setup Python and all requirements for Whisper

---
+++ installWhisper.sh
@@ -0,0 +1,20 @@
+# Installs Homebrew, Python, PyTorch, FFmpeg, Rust and Whisper
+
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Python
+brew install python
+
+# PyTorch
+pip3 install torch torchvision torchaudio
+
+# FFmpeg
+brew install ffmpeg
+
+# Rust
+pip install setuptools-rust
+
+
+# Whisper
+pip install --upgrade --no-deps --force-reinstall git+https://github.com/openai/whisper.git
